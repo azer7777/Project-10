e30331825ed16c8514f9fcb2085cc44cfa3f7588
azer7777 <azertyuio123a77@gmail.com>
2023-08-28 15:25:32 +0200

    start app projects

--- SoftDesk/SoftDesk/settings.py
+++ SoftDesk/SoftDesk/settings.py
@@ -38,9 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework.authtoken',
-    'accounts',
+    'rest_framework.authtoken',  
     'rest_framework_simplejwt',
+    'accounts',
+    'projects',
 ]
 
 
