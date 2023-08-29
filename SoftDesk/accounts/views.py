5dd195687a1c1a398b5962b59c2fb77ba324876d
azer7777 <azertyuio123a77@gmail.com>
2023-08-29 16:02:21 +0200

    start permissions

--- SoftDesk/accounts/views.py
+++ SoftDesk/accounts/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrAdmin
 from .models import CustomUser
 from .serializers import UserSerializer, UserRegistrationSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
 
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
