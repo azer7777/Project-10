a40aa04f44f68e9f40893f4bd2f451bb2f0018bb
azer7777 <azertyuio123a77@gmail.com>
2023-08-30 20:10:50 +0200

    permissions

--- SoftDesk/projects/permissions.py
+++ SoftDesk/projects/permissions.py
@@ -1,10 +1,6 @@
 from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
 
 class IsAuthorOrReadOnly(BasePermission):
-    """
-    Custom permission to only allow authors to modify or delete a resource.
-    Others can read the resource.
-    """
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
