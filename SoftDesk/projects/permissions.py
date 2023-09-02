8baf9be256b5568a9574d17e5c1e49a287199acc
azer7777 <azertyuio123a77@gmail.com>
2023-09-02 18:18:05 +0200

    projects permissions

--- SoftDesk/projects/permissions.py
+++ SoftDesk/projects/permissions.py
@@ -1,7 +1,13 @@
 from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_METHODS
-
+from rest_framework import permissions
 class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
         return obj.author == request.user
+
+
+class IsContributor(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Check if the requesting user is among the project's contributors
+        return request.user in obj.contributors.all()
--- SoftDesk/projects/views.py
+++ SoftDesk/projects/views.py
@@ -2,12 +2,21 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Project, Issue, Comment
 from .serializers import ProjectSerializer, IssueSerializer, CommentSerializer
-from .permissions import IsAuthorOrReadOnly
+from .permissions import IsAuthorOrReadOnly, IsContributor
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly, IsContributor]
+    
+    def get_queryset(self):
+        # Get the current user
+        user = self.request.user
+
+        # Filter projects where the user is a contributor
+        queryset = Project.objects.filter(contributors=user)
+
+        return queryset
 
     def perform_create(self, serializer):
         author = self.request.user
